25c46dd73da5b540fcc7e103d5f64955e6ef58e5
quandarealest <buikhacminhquan@hotmail.com>
2021-10-22 16:23:35 -0400

    finalize portfolio

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -3,10 +3,69 @@ import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <br />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range of technologies in the web development world. From Front-end to Design.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>
+            Front-end
+          </ListTitle>
+          <ListParagraph>
+            Experienced with
+            <br />
+            React.js
+            <br />
+            Redux
+            <br />
+            Saga
+            <br />
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>
+            Back-end
+          </ListTitle>
+          <ListParagraph>
+            Experienced with
+            <br />
+            Node.js
+            <br />
+            MongoDB
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>
+            Tools
+          </ListTitle>
+          <ListParagraph>
+            Experienced with
+            <br />
+            Visual Studio Code
+            <br />
+            Git
+            <br />
+            Figma
+            <br />
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
